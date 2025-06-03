a08d27cf6bb5ef68efa9ed703c8bfa7cb55898dd
zeeshu009 <138301570+zeeshu009@users.noreply.github.com>
2025-06-03 17:52:52 +0500

    Delete backend directory

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('postgres://postgres:my_password@localhost:5432/db_platform', {
-  dialect: 'postgres',
-  logging: false,
-});
-
-module.exports = { sequelize };
--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-const { registerValidator } = require('../middleware/validator');
-
-router.post('/register', registerValidator, authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
